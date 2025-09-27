ff08a1d1c7c477ce447d8b612b7f04820510a455
AK <67439739+7086600@users.noreply.github.com>
2025-09-27 15:17:32 +0300

    Add an is_canceled_87 and an is_canceled_30 column to the status temporary table

--- user_churn.sql
+++ user_churn.sql
@@ -91,3 +91,59 @@ FROM cross_join)
 SELECT *
 FROM status
 LIMIT 50;
+
+-- #6 Add an is_canceled_87 and an is_canceled_30 column to the status temporary table
+-- months temp table
+WITH months AS 
+(SELECT
+  '2017-01-01' AS first_day,
+  '2017-01-31' AS last_day
+UNION
+SELECT
+  '2017-02-01' AS first_day,
+  '2017-02-28' AS last_day
+UNION
+SELECT
+  '2017-03-01' AS first_day,
+  '2017-03-31' AS last_day),
+-- cross_join table
+cross_join AS 
+(SELECT *
+FROM subscriptions
+CROSS JOIN months),
+-- status temp table
+status AS 
+(SELECT id, STRFTIME('%m', first_day) AS 'month',
+  -- add column is_active_87 by CASE
+  CASE
+    WHEN (subscription_start < first_day)
+      AND (segment = 87
+        AND subscription_end > first_day
+            OR subscription_end IS NULL) THEN 1
+    ELSE 0    
+  END AS 'is_active_87',
+  -- add column is_active_30 by CASE
+  CASE
+    WHEN (subscription_start < first_day)
+      AND (segment = 30
+        AND subscription_end > first_day
+            OR subscription_end IS NULL) THEN 1
+    ELSE 0    
+  END AS 'is_active_30',
+  -- add column is_canceled_87 by CASE
+  CASE
+    WHEN subscription_end BETWEEN first_day AND last_day
+      THEN 1
+    ELSE 0
+  END AS 'is_canceled_87',
+  -- add column is_canceled_30 by CASE
+  CASE
+    WHEN subscription_end BETWEEN first_day AND last_day
+      THEN 1
+    ELSE 0
+  END AS 'is_canceled_30'
+FROM cross_join)
+
+SELECT *
+FROM status
+LIMIT 50;
