ae2c01419824001a5e590e8ee0db20b487d3d1dc
AK <67439739+7086600@users.noreply.github.com>
2025-09-27 14:51:29 +0300

    Create a temporary table, cross_join, from subscriptions and your months

--- user_churn.sql
+++ user_churn.sql
@@ -2,7 +2,7 @@
 -- #1  Take a look at the first 100 rows of data in the subscriptions table
  SELECT *
  FROM subscriptions
- LIMIT 5;
+ LIMIT 15;
 --  there are 2 different segments: 30 and 87
 
 -- #2 Determine the range of months of data provided. Which months will you be able to calculate churn for?
@@ -14,6 +14,22 @@ GROUP by 1;
 
 -- Calculate churn rate for each segment
 -- #3 get started, create a temporary table of months
+-- WITH months AS 
+-- (SELECT
+--   '2017-01-01' AS first_day,
+--   '2017-01-31' AS last_day
+-- UNION
+-- SELECT
+--   '2017-02-01' AS first_day,
+--   '2017-02-28' AS last_day
+-- UNION
+-- SELECT
+--   '2017-03-01' AS first_day,
+--   '2017-03-31' AS last_day)
+-- SELECT *
+-- FROM months;
+
+-- #4 Create a temporary table, cross_join, from subscriptions and your months
 WITH months AS 
 (SELECT
   '2017-01-01' AS first_day,
@@ -25,7 +41,11 @@ SELECT
 UNION
 SELECT
   '2017-03-01' AS first_day,
-  '2017-03-31' AS last_day)
+  '2017-03-31' AS last_day),
+cross_join AS 
+(SELECT *
+FROM subscriptions
+CROSS JOIN months)
 SELECT *
-FROM months;
-
+FROM cross_join
+LIMIT 50;
