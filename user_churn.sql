ea6be36b0b20758fa433c0f09bb40f3433211d9e
AK <67439739+7086600@users.noreply.github.com>
2025-09-27 14:50:35 +0300

    step 1, 2, 3: inspecting data, count months, create temporary months table

--- user_churn.sql
+++ user_churn.sql
@@ -1,2 +1,31 @@
-SELCT *
-FROM table1;
+-- Get familiar with the data
+-- #1  Take a look at the first 100 rows of data in the subscriptions table
+ SELECT *
+ FROM subscriptions
+ LIMIT 5;
+--  there are 2 different segments: 30 and 87
+
+-- #2 Determine the range of months of data provided. Which months will you be able to calculate churn for?
+SELECT STRFTIME('%m', subscription_start) AS 'month', 
+  COUNT(*)
+FROM subscriptions
+GROUP by 1;
+-- there are 4 months: dec 2016 to march 2017
+
+-- Calculate churn rate for each segment
+-- #3 get started, create a temporary table of months
+WITH months AS 
+(SELECT
+  '2017-01-01' AS first_day,
+  '2017-01-31' AS last_day
+UNION
+SELECT
+  '2017-02-01' AS first_day,
+  '2017-02-28' AS last_day
+UNION
+SELECT
+  '2017-03-01' AS first_day,
+  '2017-03-31' AS last_day)
+SELECT *
+FROM months;
+
