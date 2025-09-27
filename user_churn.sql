b9bcb8442b28b54f6a818bde96c05bcb92840f88
AK <67439739+7086600@users.noreply.github.com>
2025-09-27 16:22:23 +0300

    generalizing the CASE statements and using GROUP BY segment

--- user_churn.sql
+++ user_churn.sql
@@ -285,3 +285,50 @@ SELECT *,
   ROUND(1.0 * sum_canceled_30 / sum_active_30, 3) AS 'churn_rate_30'
 FROM status_aggregate
 LIMIT 50;
+
+-- #9 generalizing the CASE statements and using GROUP BY segment
+-- months temp table
+WITH months AS 
+(SELECT
+  '2017-01-01' AS first_day,
+  '2017-01-31' AS last_day
+UNION
+SELECT
+  '2017-02-01' AS first_day,
+  '2017-02-28' AS last_day
+UNION
+SELECT
+  '2017-03-01' AS first_day,
+  '2017-03-31' AS last_day),
+-- cross_join table
+cross_join AS 
+(SELECT *
+FROM subscriptions
+CROSS JOIN months),
+-- status temp table
+status AS 
+(SELECT id, STRFTIME('%m', first_day) AS 'month', segment,
+  -- create is_active column
+  CASE
+    WHEN (subscription_start < first_day)
+      AND (subscription_end > first_day
+            OR subscription_end IS NULL) THEN 1
+    ELSE 0    
+  END AS 'is_active',
+  -- create is_canceled column
+  CASE
+    WHEN (subscription_end BETWEEN first_day AND last_day)
+      THEN 1
+    ELSE 0
+  END AS 'is_canceled'
+FROM cross_join),
+-- status_aggregate temp table
+status_aggregate AS 
+(SELECT month, segment,
+  SUM(is_active) AS 'sum_active', 
+  SUM(is_canceled) AS 'sum_canceled'
+FROM status
+GROUP BY 1, 2)
+
+SELECT *
+FROM status_aggregate;
