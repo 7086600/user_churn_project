1a907c5883a02c4b8a5ba3c0c7af667e6682c6b1
AK <67439739+7086600@users.noreply.github.com>
2025-09-27 15:08:10 +0300

    Create a temporary tabl - status

--- user_churn.sql
+++ user_churn.sql
@@ -49,3 +49,45 @@ CROSS JOIN months)
 SELECT *
 FROM cross_join
 LIMIT 50;
+
+-- #5 Create a temporary table, status
+-- months temp table
+WITH months AS 
+(SELECT
+  '2017-01-01' AS first_day,
+  '2017-01-31' AS last_day
+UNION
+SELECT
+  '2017-02-01' AS first_day,
+  '2017-02-28' AS last_day
+UNION
+SELECT
+  '2017-03-01' AS first_day,
+  '2017-03-31' AS last_day),
+-- cross_join table
+cross_join AS 
+(SELECT *
+FROM subscriptions
+CROSS JOIN months),
+-- status temp table
+status AS 
+(SELECT id, STRFTIME('%m', first_day) AS 'month',
+  CASE
+    WHEN (subscription_start < first_day)
+      AND (segment = 87
+        AND subscription_end > first_day
+            OR subscription_end IS NULL) THEN 1
+    ELSE 0    
+  END AS 'is_active_87',
+  CASE
+    WHEN (subscription_start < first_day)
+      AND (segment = 30
+        AND subscription_end > first_day
+            OR subscription_end IS NULL) THEN 1
+    ELSE 0    
+  END AS 'is_active_30'
+FROM cross_join)
+
+SELECT *
+FROM status
+LIMIT 50;
